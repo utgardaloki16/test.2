e79997640417d4eee59b0fa13d4069fde8577550
Viacheslav Franchuk <v.franchuk@smart-soft.ru>
2023-01-04 16:08:23 +0300

    new patch-2

--- files/patch-2.c
+++ files/patch-2.c
@@ -1,6 +1,6 @@
 --- clist.c.orig	2023-01-04 14:41:14.296370300 +0300
-+++ clist.c	2023-01-04 16:00:02.146883200 +0300
-@@ -1,3 +1,23 @@
++++ clist.c	2023-01-04 16:07:19.875277300 +0300
+@@ -1,3 +1,30 @@
  #include <stdio.c>
  
  #define MAX_LISTS 255
@@ -23,4 +23,11 @@
 +    lst->tail = lst;
 +    lst->count = 1;
 +    return lst;
++}
++
++void delList(List *lst) {
++    Node *node = lst;
++    while (node->next)
++        free(node);
++    return;
 +}
