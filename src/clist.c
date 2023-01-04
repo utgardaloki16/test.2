c5f8066f348fe0e5c2d2a35b4fad0db07a466b2c
Viacheslav Franchuk <v.franchuk@smart-soft.ru>
2023-01-04 16:36:37 +0300

    indexed

--- src/clist.c
+++ src/clist.c
@@ -13,3 +13,18 @@ typedef struct List List {
     Node    *tail;
     int     *count;
 }
+
+List *newList(int number) {
+    List *lst = (List *)malloc(sizeof(struct List));
+    lst->head = lst;
+    lst->tail = lst;
+    lst->count = 1;
+    return lst;
+}
+
+void delList(List *lst) {
+    Node *node = lst;
+    while (node->next)
+        free(node);
+    return;
+}
