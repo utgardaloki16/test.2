1d1542f2d3b13a61bc907b36abdf9f60fbf3c34a
Viacheslav Franchuk <v.franchuk@smart-soft.ru>
2023-01-04 16:01:17 +0300

    changes to patch-2

--- files/patch-2.c
+++ files/patch-2.c
@@ -1,6 +1,6 @@
 --- clist.c.orig	2023-01-04 14:41:14.296370300 +0300
-+++ clist.c	2023-01-04 15:53:15.887939700 +0300
-@@ -1,3 +1,15 @@
++++ clist.c	2023-01-04 16:00:02.146883200 +0300
+@@ -1,3 +1,23 @@
  #include <stdio.c>
  
  #define MAX_LISTS 255
@@ -15,4 +15,12 @@
 +    Node    *head;
 +    Node    *tail;
 +    int     *count;
++}
++
++List *newList(int number) {
++    List *lst = (List *)malloc(sizeof(struct List));
++    lst->head = lst;
++    lst->tail = lst;
++    lst->count = 1;
++    return lst;
 +}
